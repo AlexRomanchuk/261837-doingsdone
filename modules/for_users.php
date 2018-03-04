0c8a35c8a654778083885d985554c833ad353597
Alexander <borom@inbox.ru>
2018-03-04 19:39:24 +0300

    Создан модуль добавления проекта

--- modules/for_users.php
+++ modules/for_users.php
@@ -69,30 +69,7 @@
     } elseif (isset($_POST["add"])) {
         require_once("modules/add_task.php");
     } elseif (isset($_POST["add_project"])) {
-        $newProject = $_POST;
-        if (empty($newProject["name"])) {
-            $projectErrors += ["name" => "Это поле обязательно к заполнению"];
-        } else {
-            $project = mysqli_real_escape_string($dbc, $newProject["name"]);
-            $checkQuery = "SELECT * FROM projects WHERE project_name = '$project'";
-            $check = mysqli_query($dbc, $checkQuery);
-            if (mysqli_num_rows($check) !== 0) {
-                $projectErrors += ["already_exists_name" => "Такая категория уже существует"];
-            }
-        }
-        
-        if (count($projectErrors)) {
-            $content = renderTemplate("templates/addproject.php", ["projectErrors" => $projectErrors]);
-            $className = "overlay";
-        } else {
-            $addQuery = "INSERT INTO projects SET project_name = '$project', author_id = (SELECT id FROM users WHERE email = '" . $_SESSION["user"]["email"] . "')";
-            $result = mysqli_query($dbc, $addQuery);
-            if (!$result) {
-                $content = "Произошла ошибка при добавлении проекта: " . mysqli_error($dbc);
-            } else {
-                header("Location: /");
-            }
-        }
+        require_once("modules/add_project.php");
     } else {
         $userTasks = mysqli_query($dbc, $queryTasks);
         $tasks = createArrayTasks($userTasks);
